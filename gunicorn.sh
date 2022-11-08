7db8a29a1e6bfa77801394bf29d7d3ac797c81d5
kiki-kanri <a470666@gmail.com>
2022-11-08 17:50:50 +0800

    變更python版本

--- gunicorn.sh
+++ gunicorn.sh
@@ -2,7 +2,7 @@
 . ./env.sh
 
 if [ ! -z $1 ] && [ $1 = "--dev" ]; then
-	python3.11 -m gunicorn -b $host:$port --access-logfile '-' --error-logfile '-' --reload -w 1 main.wsgi
+	python3.10 -m gunicorn -b $host:$port --access-logfile '-' --error-logfile '-' --reload -w 1 main.wsgi
 else
-	python3.11 -m gunicorn -b $host:$port -k "egg:meinheld#gunicorn_worker" -w $workers main.wsgi
+	python3.10 -m gunicorn -b $host:$port -k "egg:meinheld#gunicorn_worker" -w $workers main.wsgi
 fi
